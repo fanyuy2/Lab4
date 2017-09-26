37bfbb4ffb728aa4de1c9a830952c9bd12b904a0
yfy <yfy@wirelessprv-10-194-125-200.near.illinois.edu>
2017-09-26 16:53:36 -0500

    get started

--- src/main/java/ReclamationProject.java
+++ src/main/java/ReclamationProject.java
@@ -7,20 +7,49 @@
  * 4. Add comments and Javadoc comments where needed
  * 5. Remove unnecessary comments as appropriate
  */
-
-public class ReclamationProject
-{
-    static String doit(String a,String b){
-        if (a.length() > b.length()){
-            String c = a; // TODO: set c to a
-            a=b; b=c;}
-        String r = (a.equals(b)) ? "" : ""; // I love the ternary operator!
+/**
+ * @author yfy
+ *
+ */
+public class ReclamationProject {
+    /**
+     *
+     * @param args a
+     */
+    public static void main(final String[] args) {
+        System.out.println(doit("homework", "homework sucks!"));
+    }
+    /**
+     *
+     * @param a longer string
+     * @param b shorter string
+     * @return same consecutive combined in a and b
+     */
+    static String doit(final String a, final String b) {
+        //if a has more characters than b, exchange the value of a and b
+        String shorterString = "";
+        String longerString = "";
+        if (a.length() > b.length()) {
+            longerString = a;
+            shorterString  = b;
+        }
+        String sameCharactersCombo = "";
+        if (a.equals(b)) {
+            sameCharactersCombo = "";
+        }
         /*
          * For loop with i
          */
-        for (int i = 0; i < a.length(); i++) { for (int j = a.length() - i; j > 0; j--) {
-                for (int k = 0; k < b.length()- j; k++) {
-                    r = (a.regionMatches(i, b, k, j) && j >r.length()) ? a.substring(i,i + j) : r; // Do it!
-                        }} // Ah yeah
-        } return r; }
+        for (int i = 0; i < shorterString.length(); i++) {
+            for (int j = shorterString.length() - i; j > 0; j--) {
+                for (int k = 0; k < longerString.length() - j; k++) {
+                    if (shorterString.regionMatches(i, longerString, k, j)
+                            && j > sameCharactersCombo.length()) {
+                        sameCharactersCombo = shorterString.substring(i, i + j);
+                    }
+                } // Ah yeah
+            }
+        }
+        return sameCharactersCombo;
+    }
 }
